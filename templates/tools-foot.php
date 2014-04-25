066be3267c44e887765ca3ad75c7366b18a2b93f
wearnshaw <wearnshaw@gmail.com>
2014-04-25 16:16:19 -0600

    Styling

--- templates/tools-foot.php
+++ templates/tools-foot.php
@@ -1,6 +1,6 @@
 </div>
-</div>
 <div id="sp_footer">
 <div id="sp_footer_content">
+</div>
 </div>
 </div>
--- templates/tools-head.php
+++ templates/tools-head.php
@@ -1,13 +1,15 @@
 <div id="sp_header">
 <div id="sp_header_content">
+<div id="sp_logo" class="cutout inshadow"></div>
+<div id="sp_ad_banner" class="cutout">
+<?php include ROOT.'/ads/banner.php'; ?>
+<div id="sp_ad_banner_shadow" class="inshadow"></div>
+</div>
 <div id="sp_menu">
 <?php include ROOT.'/menus/tools.php'; ?>
 </div>
-<div id="sp_ad_banner">
-<?php include ROOT.'/ads/banner.php'; ?>
-<div id="sp_ad_banner_shadow"></div>
-</div>
+<div class="clear"></div>
 </div>
 </div>
 <div id="sp_page">
-<div id="sp_page_content">
+<div id="sp_page_content" class="inshadow">
